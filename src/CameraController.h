38b04e1632b527c277bbb01b25c120827b6f2089
Asher Baker <asherkin@gmail.com>
2015-04-10 15:53:02 +0100

    Room labels.

--- src/CameraController.h
+++ src/CameraController.h
@@ -2,23 +2,32 @@
 
 #include "LogicComponent.h"
 
+namespace Urho3D {
+	class UIElement;
+	class Text;
+}
+
 class CameraController: public Urho3D::LogicComponent
 {
-    OBJECT(CameraController)
+	OBJECT(CameraController)
 
 public:
-    CameraController(Urho3D::Context *context);
-    static void RegisterObject(Urho3D::Context *context);
+	CameraController(Urho3D::Context *context);
+	static void RegisterObject(Urho3D::Context *context);
     
 public:
-    void DelayedStart();
-    void Update(float timeStep);
+	void DelayedStart();
+	void Update(float timeStep);
 
 public:
-    float GetYawAngle();
+	float GetYawAngle();
 
 private:
 	Urho3D::IntVector2 cameraRoom_;
-    float cameraYaw_;
-    float targetCameraYaw_;
+	float cameraYaw_;
+	float targetCameraYaw_;
+	bool showPanel_;
+	float panelYPosition_;
+	Urho3D::UIElement *panel_;
+	Urho3D::Text *label_;
 };
