cfd370af841ccc81f544e12523f5fb2fdfd77f21
Keir Sweeney <k1214635@kingston.ac.uk>
2015-04-12 12:43:14 +0100

    Big chunk of work for you to know which terminal is below which glyph, but i think its all working okay.

--- src/MrWright.cpp
+++ src/MrWright.cpp
@@ -21,6 +21,7 @@
 #include "Model.h"
 #include "Material.h"
 #include "Input.h"
+#include "Terminal.h"
 
 
 using namespace Urho3D;
@@ -68,33 +69,79 @@ void MrWright::Update(float timeStep)
 	swapTimer_ = 0.0f;
 
 	Node *roomNode = node_->GetParent();
-	PODVector<Node *> nodes;
-	roomNode->GetChildrenWithComponent<InteractablePoster>(nodes, true);
 
-	if (nodes.Empty()) {
-		LOGERROR("ITS EMPTY");
+	PODVector<Node *> posterNodes;
+	PODVector<Node *> terminalNodes;
+
+	roomNode->GetChildrenWithComponent<InteractablePoster>(posterNodes, true);
+	roomNode->GetChildrenWithComponent<Terminal>(terminalNodes, true);
+
+	if (posterNodes.Empty()) {
+		LOGERROR("Poster nodes are empty");
 	}
-	
+
+	if (terminalNodes.Empty()) {
+		LOGERROR("Terminal nodes are empty");
+	}
 	
 	ResourceCache *cache = GetSubsystem<ResourceCache>();
 
-
-	int m = nodes.Size();
+	int m = posterNodes.Size();
 	while (m > 0) {
 		int n = Random(0, m--);
 
-		StaticModel *a = nodes[m]->GetComponent<StaticModel>();
-		StaticModel *b = nodes[n]->GetComponent<StaticModel>();
+		StaticModel *a = posterNodes[m]->GetComponent<StaticModel>();
+		StaticModel *b = posterNodes[n]->GetComponent<StaticModel>();
 
 		Material *t = a->GetMaterial();
 		a->SetMaterial(b->GetMaterial());
 		b->SetMaterial(t);
 	}
-			
 	
+	int x = terminalNodes.Size();
+	LOGERRORF("terminal nodes size: %d", x);
+	x -= 1;
+	int n = posterNodes.Size();
+	LOGERRORF("poster nodes size %d", n);
 
-	for (PODVector<Node *>::ConstIterator i = nodes.Begin(); i != nodes.End(); ++i) {
-		Node *posterNode = *i;
+	while (x >= 0) {
+
+		
+		
+			for (PODVector<Node *>::ConstIterator i = posterNodes.Begin(); i != posterNodes.End(); ++i) {
+
+
+				Node *thisPosterNodes = *i;
+				// at this point, were checing for the first terminal[5] and going through each poster to see if the diff is less than 0.5
+				Vector3 thisTerminalPos = terminalNodes[x]->GetWorldPosition();
+				Vector3 thisPosterPos = thisPosterNodes->GetWorldPosition();
+				Vector3 diff = thisPosterPos - thisTerminalPos;
+				float calc = abs(diff.LengthSquared());
+				LOGERRORF("diff %f", calc);
+
+				if (abs(diff.LengthSquared()) > 1.0f * 1.0f) {
+					LOGERROR("Not Closest terminal");
+					//break;// its not the closest 
+				}
+				else {
+					StaticModel *c = thisPosterNodes->GetComponent<StaticModel>();
+					LOGERROR("Found terminal");
+					c->GetMaterial(); //this is the current material above the closest terminal?
+					x--;
+					if (x < 0) {
+						goto LABEL;
+					}
+					//thisPosterNodes->Remove();
+					//do what we need with it then break out
+				}
+				LOGERRORF("x: %d", x);
+			}
+		
+	}
+	
+	LABEL:
+	for (PODVector<Node *>::ConstIterator i = posterNodes.Begin(); i != posterNodes.End(); ++i) {
+		Node *thisPosterNode = *i;
 
 		
 			
