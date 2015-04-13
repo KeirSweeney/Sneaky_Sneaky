8a5a798546f48d33b8d3ed1978efeef3462338f3
Leon <k1223392@kingston.ac.uk>
2015-04-13 16:06:18 +0100

    Security Camera Implementation

--- src/SecurityCamera.cpp
+++ src/SecurityCamera.cpp
@@ -4,13 +4,20 @@
 #include "Node.h"
 #include "Scene.h"
 #include "Log.h"
+#include "Octree.h"
+#include "DebugRenderer.h"
+#include "Frustum.h"
+#include "Guard.h"
+#include "Light.h"
+#include "NavigationMesh.h"
+
 
 using namespace Urho3D;
 
 SecurityCamera::SecurityCamera(Context *context) :
     InteractableComponent(context),
     sweepingBack_(false),
-    pitch_(-15.0f), yaw_(0.0f)
+    pitch_(-30.0f), yaw_(0.0f)
 {
 }
 
@@ -27,6 +34,7 @@ void SecurityCamera::DelayedStart()
 
 void SecurityCamera::Update(float timeStep)
 {
+	Node *personNode = GetScene()->GetChild("Person", true);
     yaw_ += (sweepingBack_ ? -1.0f : 1.0f) * 20.0f * timeStep;
 
     if (abs(yaw_) >= 45.0f) {
@@ -36,4 +44,88 @@ void SecurityCamera::Update(float timeStep)
     node_->SetWorldRotation(startRotation_);
     node_->Rotate(Quaternion(pitch_, Vector3::LEFT));
     node_->Rotate(Quaternion(yaw_, Vector3::UP));
+	
+	SearchForPlayer(personNode);
+}
+
+bool SecurityCamera::SearchForPlayer(Node* player)
+{
+	DebugRenderer *debug = node_->GetScene()->GetComponent<DebugRenderer>();
+
+	Vector3 cameraPosition = node_->GetWorldPosition();
+	Vector3 cameraDirection = node_->GetWorldDirection();
+	Vector3 playerPosition = player->GetWorldPosition();
+	Vector3 difference = (playerPosition - cameraPosition);
+
+	difference.Normalize();
+
+	Frustum frustrum;
+
+	frustrum.Define(30.0f / 2.0f, 1.0f, 1.0f, M_MIN_NEARCLIP, 10.0f, Matrix3x4(cameraPosition, node_->GetWorldRotation(), 1.0f));
+
+	debug->AddFrustum(frustrum, Color::BLUE);
+
+	if (cameraDirection.DotProduct(difference) < Cos(30.0f / 2.0f))
+	{
+		Node *roomNode = node_->GetParent()->GetParent();
+
+		PODVector<Node *> nodes;
+
+		roomNode->GetChildrenWithComponent<Light>(nodes, true);
+
+		for (PODVector<Node *>::ConstIterator i = nodes.Begin(); i != nodes.End(); ++i) {
+			Node *lightNode = *i;
+
+			Light *light = lightNode->GetComponent<Light>();
+
+			light->SetColor(Color::WHITE);
+		}
+		return false;
+	}
+
+	Ray ray(cameraPosition + Vector3(0.0f, 4.6f, 0.0f), difference);
+	PODVector<RayQueryResult> result;
+	RayOctreeQuery query(result, ray, RAY_TRIANGLE, DRAWABLE_GEOMETRY);
+
+	Octree *octree = GetScene()->GetComponent<Octree>();
+	octree->RaycastSingle(query);
+
+	debug->AddLine(cameraPosition, playerPosition, Color::BLUE);
+
+	AlertGuards();
+
+	return true;
+}
+
+void SecurityCamera::AlertGuards()
+{
+	Node *personNode = GetScene()->GetChild("Person", true);
+
+	Node *roomNode = node_->GetParent()->GetParent();
+	PODVector<Node *> nodes;
+
+	roomNode->GetChildrenWithComponent<Light>(nodes, true);
+
+	for (PODVector<Node *>::ConstIterator i = nodes.Begin(); i != nodes.End(); ++i) {
+		Node *lightNode = *i;
+
+		Light *light = lightNode->GetComponent<Light>();
+
+		light->SetColor(Color::RED);
+	}
+
+	NavigationMesh *navMesh = GetScene()->GetComponent<NavigationMesh>();
+
+	Vector3 pointPath = personNode->GetWorldPosition();
+
+	roomNode->GetChildrenWithComponent<Guard>(nodes, true);
+
+	for (PODVector<Node *>::ConstIterator i = nodes.Begin(); i != nodes.End(); ++i) {
+		Node *guardNode = *i;
+
+		Guard *guard = guardNode->GetComponent<Guard>();
+
+		navMesh->FindPath(path_, guardNode->GetWorldPosition(), pointPath);
+		guard->SetPath(path_);
+	}
 }
