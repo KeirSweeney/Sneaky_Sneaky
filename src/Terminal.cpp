165d6efd46a78d3398ca377073834907c1fd288b
Asher Baker <asherkin@limetech.org>
2015-04-16 17:36:10 +0100

    Don't take vertical difference into account for terminal interaction cone check.

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -23,89 +23,90 @@ const float Terminal::VIEW_DISTANCE = 2.0f;
 const float Terminal::VIEW_ANGLE = 90.0f;
 
 Terminal::Terminal(Context *context):
-    InteractableComponent(context),
-    displayWidth_(200), displayHeight_(100), content_("Lemons.")
+	InteractableComponent(context),
+	displayWidth_(200), displayHeight_(100), content_("Lemons.")
 {
 }
 
 void Terminal::RegisterObject(Context* context)
 {
-    context->RegisterFactory<Terminal>("Logic");
+	context->RegisterFactory<Terminal>("Logic");
 
-    COPY_BASE_ATTRIBUTES(InteractableComponent);
+	COPY_BASE_ATTRIBUTES(InteractableComponent);
 }
 
 void Terminal::LoadFromXML(const XMLElement &xml)
 {
-    if (xml.HasAttribute("displaysize")) {
-        IntVector2 displaySize = xml.GetIntVector2("displaysize");
-        displayWidth_ = displaySize.x_;
-        displayHeight_ = displaySize.y_;
-    }
+	if (xml.HasAttribute("displaysize")) {
+		IntVector2 displaySize = xml.GetIntVector2("displaysize");
+		displayWidth_ = displaySize.x_;
+		displayHeight_ = displaySize.y_;
+	}
 
-    const String content = xml.GetValue();
-    if (!content.Empty()) {
-        content_ = content;
-    }
+	const String content = xml.GetValue();
+	if (!content.Empty()) {
+		content_ = content;
+	}
 }
 
 void Terminal::DelayedStart()
 {
-    UI *ui = GetSubsystem<UI>();
+	UI *ui = GetSubsystem<UI>();
 
-    panel_ = ui->GetRoot()->CreateChild<UIElement>();
-    panel_->SetFixedSize(displayWidth_, displayHeight_);
-    panel_->SetVisible(false);
+	panel_ = ui->GetRoot()->CreateChild<UIElement>();
+	panel_->SetFixedSize(displayWidth_, displayHeight_);
+	panel_->SetVisible(false);
 
-    Sprite *background = panel_->CreateChild<Sprite>();
-    background->SetFixedSize(panel_->GetSize());
-    background->SetColor(Color::BLACK);
-    background->SetOpacity(0.6f);
+	Sprite *background = panel_->CreateChild<Sprite>();
+	background->SetFixedSize(panel_->GetSize());
+	background->SetColor(Color::BLACK);
+	background->SetOpacity(0.6f);
 
-    Text *label = panel_->CreateChild<Text>();
-    label->SetFixedSize(panel_->GetSize() - IntVector2(20, 20));
-    label->SetFont("Fonts/Anonymous Pro.ttf");
-    label->SetColor(Color::WHITE);
-    label->SetWordwrap(true);
-    label->SetText(content_);
-    label->SetAlignment(HA_CENTER, VA_CENTER);
+	Text *label = panel_->CreateChild<Text>();
+	label->SetFixedSize(panel_->GetSize() - IntVector2(20, 20));
+	label->SetFont("Fonts/Anonymous Pro.ttf");
+	label->SetColor(Color::WHITE);
+	label->SetWordwrap(true);
+	label->SetText(content_);
+	label->SetAlignment(HA_CENTER, VA_CENTER);
 }
 
 void Terminal::Update(float timeStep)
 {
-    Vector3 position = node_->GetWorldPosition();
+	Vector3 position = node_->GetWorldPosition();
 
-    if (!CanPlayerInteract()) {
-        panel_->SetVisible(false);
-        return;
-    }
+	if (!CanPlayerInteract()) {
+		panel_->SetVisible(false);
+		return;
+	}
 
-    Renderer *renderer = GetSubsystem<Renderer>();
-    Camera *camera = renderer->GetViewport(0)->GetCamera();
-    Vector2 screenPosition = camera->WorldToScreenPoint(position);
+	Renderer *renderer = GetSubsystem<Renderer>();
+	Camera *camera = renderer->GetViewport(0)->GetCamera();
+	Vector2 screenPosition = camera->WorldToScreenPoint(position);
 
-    Graphics *graphics = GetSubsystem<Graphics>();
-    IntVector2 uiPosition;
-    uiPosition.x_ = (int)round((screenPosition.x_ * graphics->GetWidth()) - (panel_->GetWidth() / 2.0f));
-    uiPosition.y_ = (int)round((screenPosition.y_ * graphics->GetHeight()) - (panel_->GetHeight() / 2.0f));
+	Graphics *graphics = GetSubsystem<Graphics>();
+	IntVector2 uiPosition;
+	uiPosition.x_ = (int)round((screenPosition.x_ * graphics->GetWidth()) - (panel_->GetWidth() / 2.0f));
+	uiPosition.y_ = (int)round((screenPosition.y_ * graphics->GetHeight()) - (panel_->GetHeight() / 2.0f));
 
-    panel_->SetPosition(uiPosition);
-    panel_->SetVisible(true);
+	panel_->SetPosition(uiPosition);
+	panel_->SetVisible(true);
 }
 
 bool Terminal::CanPlayerInteract()
 {
-    Vector3 terminalPosition = node_->GetWorldPosition();
-    Vector3 personPosition = GetScene()->GetChild("Person", true)->GetWorldPosition();
+	Vector3 terminalPosition = node_->GetWorldPosition();
+	Vector3 personPosition = GetScene()->GetChild("Person", true)->GetWorldPosition();
 
-    Vector3 difference = personPosition - terminalPosition;
+	Vector3 difference = personPosition - terminalPosition;
+	difference.y_ = 0.0f;
 
-    if(difference.LengthSquared() > (VIEW_DISTANCE * VIEW_DISTANCE)) {
-        return false;
-    }
+	if(difference.LengthSquared() > (VIEW_DISTANCE * VIEW_DISTANCE)) {
+		return false;
+	}
 
-    Vector3 forward = -node_->GetWorldDirection();
-    difference.Normalize();
+	Vector3 forward = -node_->GetWorldDirection();
+	difference.Normalize();
 
 #if 0
 	DebugRenderer *debug = node_->GetScene()->GetComponent<DebugRenderer>();
@@ -113,9 +114,9 @@ bool Terminal::CanPlayerInteract()
 	debug->AddLine(terminalPosition, terminalPosition + difference, Color::RED);
 #endif
 
-    if (forward.DotProduct(difference) < Cos(VIEW_ANGLE / 2.0f)) {
-        return false;
-    }
+	if (forward.DotProduct(difference) < Cos(VIEW_ANGLE / 2.0f)) {
+		return false;
+	}
 
-    return true;
+	return true;
 }
