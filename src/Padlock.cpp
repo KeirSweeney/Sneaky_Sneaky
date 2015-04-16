42e201884336df22d2e25d77145df6ec4ac61fea
Asher Baker <asherkin@limetech.org>
2015-04-16 17:36:28 +0100

    Only check distance for padlock removal.

--- src/Padlock.cpp
+++ src/Padlock.cpp
@@ -24,90 +24,89 @@
 
 using namespace Urho3D;
 
-const float Padlock::VIEW_DISTANCE = 2.0f;
-const float Padlock::VIEW_ANGLE = 90.0f;
+const float Padlock::DISTANCE = 1.0f;
 
 Padlock::Padlock(Context *context):
-    InteractableComponent(context),
-    displayWidth_(200), displayHeight_(100), content_("Lemons.")
+	InteractableComponent(context),
+	displayWidth_(200), displayHeight_(100), content_("Lemons.")
 {
 }
 
 void Padlock::RegisterObject(Context* context)
 {
-    context->RegisterFactory<Padlock>("Logic");
+	context->RegisterFactory<Padlock>("Logic");
 
-    COPY_BASE_ATTRIBUTES(InteractableComponent);
+	COPY_BASE_ATTRIBUTES(InteractableComponent);
 }
 
 void Padlock::LoadFromXML(const XMLElement &xml)
 {
-    if (xml.HasAttribute("displaysize")) {
-        IntVector2 displaySize = xml.GetIntVector2("displaysize");
-        displayWidth_ = displaySize.x_;
-        displayHeight_ = displaySize.y_;
-    }
+	if (xml.HasAttribute("displaysize")) {
+		IntVector2 displaySize = xml.GetIntVector2("displaysize");
+		displayWidth_ = displaySize.x_;
+		displayHeight_ = displaySize.y_;
+	}
 
-    const String content = xml.GetValue();
-    if (!content.Empty()) {
-        content_ = content;
-    }
+	const String content = xml.GetValue();
+	if (!content.Empty()) {
+		content_ = content;
+	}
 }
 
 void Padlock::DelayedStart()
 {
-    UI *ui = GetSubsystem<UI>();
+	UI *ui = GetSubsystem<UI>();
 
-    panel_ = ui->GetRoot()->CreateChild<UIElement>();
-    panel_->SetFixedSize(displayWidth_, displayHeight_);
-    panel_->SetVisible(false);
+	panel_ = ui->GetRoot()->CreateChild<UIElement>();
+	panel_->SetFixedSize(displayWidth_, displayHeight_);
+	panel_->SetVisible(false);
 
-    Sprite *background = panel_->CreateChild<Sprite>();
-    background->SetFixedSize(panel_->GetSize());
-    background->SetColor(Color::BLACK);
-    background->SetOpacity(0.6f);
+	Sprite *background = panel_->CreateChild<Sprite>();
+	background->SetFixedSize(panel_->GetSize());
+	background->SetColor(Color::BLACK);
+	background->SetOpacity(0.6f);
 
-    Text *label = panel_->CreateChild<Text>();
-    label->SetFixedSize(panel_->GetSize() - IntVector2(20, 20));
-    label->SetFont("Fonts/Anonymous Pro.ttf");
-    label->SetColor(Color::WHITE);
-    label->SetWordwrap(true);
-    label->SetText(content_);
-    label->SetAlignment(HA_CENTER, VA_CENTER);
+	Text *label = panel_->CreateChild<Text>();
+	label->SetFixedSize(panel_->GetSize() - IntVector2(20, 20));
+	label->SetFont("Fonts/Anonymous Pro.ttf");
+	label->SetColor(Color::WHITE);
+	label->SetWordwrap(true);
+	label->SetText(content_);
+	label->SetAlignment(HA_CENTER, VA_CENTER);
 }
 
 void Padlock::Update(float timeStep)
 {
-    Vector3 position = node_->GetWorldPosition();
+	Vector3 position = node_->GetWorldPosition();
 
-    if (!CanPlayerInteract()) {
-        panel_->SetVisible(false);
-        return;
-    }
+	if (!CanPlayerInteract()) {
+		panel_->SetVisible(false);
+		return;
+	}
 
-    Renderer *renderer = GetSubsystem<Renderer>();
-    Camera *camera = renderer->GetViewport(0)->GetCamera();
-    Vector2 screenPosition = camera->WorldToScreenPoint(position);
+	Renderer *renderer = GetSubsystem<Renderer>();
+	Camera *camera = renderer->GetViewport(0)->GetCamera();
+	Vector2 screenPosition = camera->WorldToScreenPoint(position);
 
-    Graphics *graphics = GetSubsystem<Graphics>();
-    IntVector2 uiPosition;
-    uiPosition.x_ = (int)round((screenPosition.x_ * graphics->GetWidth()) - (panel_->GetWidth() / 2.0f));
-    uiPosition.y_ = (int)round((screenPosition.y_ * graphics->GetHeight()) - (panel_->GetHeight() / 2.0f));
+	Graphics *graphics = GetSubsystem<Graphics>();
+	IntVector2 uiPosition;
+	uiPosition.x_ = (int)round((screenPosition.x_ * graphics->GetWidth()) - (panel_->GetWidth() / 2.0f));
+	uiPosition.y_ = (int)round((screenPosition.y_ * graphics->GetHeight()) - (panel_->GetHeight() / 2.0f));
 
-    panel_->SetPosition(uiPosition);
-    panel_->SetVisible(true);
+	panel_->SetPosition(uiPosition);
+	panel_->SetVisible(true);
 
-    Node *personNode = GetScene()->GetChild("Person", true);
+	Node *personNode = GetScene()->GetChild("Person", true);
 	Inventory *inventory = personNode->GetComponent<Inventory>();
 
 	const Vector<SharedPtr<Pickup>> &items = inventory->GetItems();
 
-    for (Vector<SharedPtr<Pickup>>::ConstIterator i = items.Begin(); i != items.End(); ++i) {
-        Urho3D::SharedPtr<Pickup> item = *i;
+	for (Vector<SharedPtr<Pickup>>::ConstIterator i = items.Begin(); i != items.End(); ++i) {
+		Urho3D::SharedPtr<Pickup> item = *i;
 
 		if(item->GetPickupType() != content_) {
 			continue;
-  }
+		}
 
 		node_->SetEnabled(false);
 		panel_->SetVisible(false);
@@ -117,24 +116,13 @@ void Padlock::Update(float timeStep)
 
 bool Padlock::CanPlayerInteract()
 {
-    Vector3 PadlockPosition = node_->GetWorldPosition();
-    Vector3 personPosition = GetScene()->GetChild("Person", true)->GetWorldPosition();
-    Vector3 difference = personPosition - PadlockPosition;
+	Vector3 PadlockPosition = node_->GetWorldPosition();
+	Vector3 personPosition = GetScene()->GetChild("Person", true)->GetWorldPosition();
+	Vector3 difference = personPosition - PadlockPosition;
 
-    if(difference.LengthSquared() > (VIEW_DISTANCE * VIEW_DISTANCE)) {
-        return false;
-    }
+	if(difference.LengthSquared() > (DISTANCE * DISTANCE)) {
+		return false;
+	}
 
-    Vector3 forward = -node_->GetWorldDirection();
-    difference.Normalize();
-
-    //DebugRenderer *debug = node_->GetScene()->GetComponent<DebugRenderer>();
-    //debug->AddLine(PadlockPosition, PadlockPosition + forward, Color::BLUE);
-    //debug->AddLine(PadlockPosition, PadlockPosition + difference, Color::RED);
-
-    if (forward.DotProduct(difference) < Cos(VIEW_ANGLE / 2.0f)) {
-        return false;
-    }
-
-    return true;
+	return true;
 }
--- src/Padlock.h
+++ src/Padlock.h
@@ -3,35 +3,34 @@
 #include "InteractableComponent.h"
 
 namespace Urho3D {
-    class UIElement;
+	class UIElement;
 }
 
 class Pickup;
 
 class Padlock: public InteractableComponent
 {
-    OBJECT(Padlock)
+	OBJECT(Padlock)
 
 private:
-    static const float VIEW_DISTANCE;
-    static const float VIEW_ANGLE;
+	static const float DISTANCE;
 
 public:
-    Padlock(Urho3D::Context *context);
-    static void RegisterObject(Urho3D::Context *context);
+	Padlock(Urho3D::Context *context);
+	static void RegisterObject(Urho3D::Context *context);
 
-    void LoadFromXML(const Urho3D::XMLElement &xml);
-    
+	void LoadFromXML(const Urho3D::XMLElement &xml);
+
 public:
-    void DelayedStart();
-    void Update(float timeStep);
+	void DelayedStart();
+	void Update(float timeStep);
 
 private:
-    bool CanPlayerInteract();
+	bool CanPlayerInteract();
 
 private:
-    int displayWidth_;
-    int displayHeight_;
-    Urho3D::String content_;
-    Urho3D::UIElement *panel_;
+	int displayWidth_;
+	int displayHeight_;
+	Urho3D::String content_;
+	Urho3D::UIElement *panel_;
 };
