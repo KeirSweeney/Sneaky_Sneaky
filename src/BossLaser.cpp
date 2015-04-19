1afa47032383b3418ee3e66dfebd383c9065dd76
Leon <k1223392@kingston.ac.uk>
2015-04-19 18:51:22 +0100

    BossLaser implementation

---
+++ src/BossLaser.cpp
@@ -0,0 +1,95 @@
+#include "BossLaser.h"
+
+#include "CameraController.h"
+#include "Game.h"
+
+#include "Camera.h"
+#include "Context.h"
+#include "DebugRenderer.h"
+#include "Graphics.h"
+#include "Input.h"
+#include "Light.h"
+#include "Log.h"
+#include "Material.h"
+#include "NavigationMesh.h"
+#include "Node.h"
+#include "Octree.h"
+#include "PhysicsEvents.h"
+#include "Renderer.h"
+#include "ResourceCache.h"
+#include "RigidBody.h"
+#include "Scene.h"
+#include "Sound.h"
+#include "SoundSource.h"
+#include "StaticModel.h"
+#include "UI.h"
+#include "Person.h"
+
+
+using namespace Urho3D;
+
+
+BossLaser::BossLaser(Context *context):
+	InteractableComponent(context),
+	lightPulse_(false),
+	lightTime_(0.0f), laserTime_(0.0f),
+	laserInterval_(0.0f), laserDelay_(0.0f)
+{}
+
+void BossLaser::RegisterObject(Context *context)
+{
+	context->RegisterFactory<BossLaser>("Logic");
+	COPY_BASE_ATTRIBUTES(InteractableComponent);
+}
+
+void BossLaser::LoadFromXML(const XMLElement &xml)
+{
+	laserInterval_ = xml.GetFloat("interval");
+	laserDelay_ = xml.GetFloat("delay");
+}
+
+void BossLaser::DelayedStart()
+{
+	source_ = node_->CreateComponent<SoundSource>();
+
+	model_ = node_->GetComponent<StaticModel>();
+	model_->SetEnabled(false);
+
+	rigidBody_ = node_->GetComponent<RigidBody>();
+	rigidBody_->SetTrigger(true);
+
+	SubscribeToEvent(node_, E_NODECOLLISION, HANDLER(BossLaser, HandleNodeCollision));
+}
+
+
+void BossLaser::Update(float timeStep)
+{
+
+	if (laserInterval_ > 0.0f) {
+		laserTime_ += timeStep;
+
+		if (laserTime_ > laserInterval_) {
+			model_->SetEnabled(!model_->IsEnabled());
+			laserTime_ = 0.0f;
+		}
+	}
+
+}
+
+
+void BossLaser::HandleNodeCollision(StringHash eventType, VariantMap &eventData)
+{
+	Node *personNode = GetScene()->GetChild("Person", true);
+	Person *person = personNode->GetComponent<Person>();
+	Node *other = (Node *)eventData[NodeCollision::P_OTHERNODE].GetPtr();
+
+	if (other != personNode) {
+		return;
+	}
+
+	lightPulse_ = true;
+
+	//Deduct player hp
+	person->TakeDamage();
+
+}
